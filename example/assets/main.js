69b0f42adb4f595f98491b05592132ec382ad084
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-07-26 15:20:09 +0330

    feat: complete example file

---
+++ example/assets/main.js
@@ -0,0 +1,32 @@
+import unoJS from "../../dist/esm/index.js";
+// Usage example:
+const options = {
+  user: {
+    fullName: "John Doe",
+    email: "j.doe@example.com",
+    avatar: null,
+  },
+  autoSecretKey: "secret",
+  callbacks: {
+    onOpenWidget: () => console.log("Widget opened"),
+    onCloseWidget: () => console.log("Widget closed"),
+    onStartMask: () => console.log("Started mask"),
+    onStopMask: () => console.log("Stopped mask"),
+    onStartTimer: ({ second, minute, hours }) =>
+      console.log(
+        "Started recording timer: " + second + ":" + minute + ":" + hours
+      ),
+    onStopTimer: () => console.log("Stopped recording timer"),
+    onStartRecording: () => console.log("Started recording!"),
+    onStopRecording: () => console.log("Stopped recording!"),
+    onSubmit: () => console.log("Submitted!"),
+    onError: () => console.log("Error!"),
+  },
+};
+const subscriptionData = {
+  apiKey: "#API_KEY#",
+  requestUrl: "#REQUEST_URL#",
+};
+
+const startButtonId = "start-btn";
+unoJS.initialize(startButtonId, subscriptionData, options);
