90c67d69400db849f2379bac10ac51edcb34e8ea
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-07-31 11:32:55 +0330

    feat: change webpack and dev command

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "none",
+  useTabs: false,
+  singleQuote: false,
+  bracketSpacing: false,
+  arrowParens: "avoid",
+  proseWrap: "never",
+  requirePragma: false
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,61 @@
+const path = require('path');
+
 module.exports = {
-  entry: "./src/styles/index.scss",
+  entry: './src/styles/index.scss',
   output: {
-    path: "./dist",
-    filename: "[name].css",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].css',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader", // creates style nodes from JS strings
+          'style-loader', // creates style nodes from JS strings
           {
-            loader: "css-loader", // translates CSS into CommonJS
+            loader: 'css-loader', // translates CSS into CommonJS
             options: {
-              module: true,
-              localIdentName: "[name]__[local]___[hash:base64:5]",
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
             },
           },
-          "sass-loader", // compiles Sass to CSS
+          'sass-loader', // compiles Sass to CSS
         ],
       },
     ],
   },
+  // Configure the "webpack-dev-server" plugin
+  devServer: {
+    static: [
+      {
+        directory: path.resolve(process.cwd(), 'dist'),
+        publicPath: 'dist',
+      },
+      {
+        directory: path.resolve(process.cwd(), 'example'),
+      },
+    ],
+    devMiddleware: {
+      index: true,
+      mimeTypes: {
+        '': 'text/javascript',
+        false: 'text/javascript',
+        html: 'text/javascript',
+        null: 'text/javascript',
+        default: 'text/javascript',
+        js: 'text/javascript'
+      },
+      publicPath: 'dist',
+      serverSideRender: true,
+      writeToDisk: true,
+    },
+    watchFiles: [path.resolve(process.cwd(), 'example/index.html')],
+    magicHtml: true,
+    compress: false,
+    port: 5456,
+    hot: true,
+    open: true,
+  },
+  mode: 'development',
 };
