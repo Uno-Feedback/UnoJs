d755a4435d7240e414640f46ed09b3f6cdb70ca7
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-07-31 15:20:11 +0330

    fix: init element every click

--- src/components/recordWidget/index.ts
+++ src/components/recordWidget/index.ts
@@ -11,60 +11,73 @@ import {
 
 // wrapper element
 const wrapper = document.createElement("div");
-wrapper.setAttribute("id", "uno-record-widget");
-wrapper.classList.add("uno-record-wrapper");
 // Timer element
 const timerWrapper = document.createElement("div");
-timerWrapper.classList.add("uno-timer");
-wrapper.appendChild(timerWrapper);
 const counter = document.createElement("span");
-counter.classList.add("uno-timer-counter");
-timerWrapper.appendChild(counter);
 // Record Elements
 const recordWrapper = document.createElement("div");
-recordWrapper.classList.add("uno-record");
-wrapper.appendChild(recordWrapper);
 const startRecordButton = document.createElement("span");
-startRecordButton.classList.add("uno-record-start");
-startRecordButton.innerHTML = recordIcon;
-recordWrapper.appendChild(startRecordButton);
 const stopRecordButton = document.createElement("span");
-stopRecordButton.classList.add("uno-record-stop");
-stopRecordButton.innerHTML = stopRecordIcon;
 const recordingButton = document.createElement("span");
-recordingButton.classList.add("id", "uno-recording");
-recordingButton.innerHTML = recordingIcon;
-timerWrapper.appendChild(recordingButton);
 // Mic Element
 const muteWrapper = document.createElement("div");
-muteWrapper.classList.add("uno-mute");
-wrapper.appendChild(muteWrapper);
 const muteStartButton = document.createElement("span");
-muteStartButton.classList.add("uno-mute-start");
-muteStartButton.innerHTML = muteIcon;
-muteWrapper.appendChild(muteStartButton);
 const muteStopButton = document.createElement("span");
-muteStopButton.classList.add("uno-mute-stop");
-muteStopButton.innerHTML = unmuteIcon;
 // Mask Element
 const maskWrapper = document.createElement("div");
-maskWrapper.classList.add("uno-mask");
-wrapper.appendChild(maskWrapper);
 const maskStartButton = document.createElement("span");
-maskStartButton.classList.add("uno-mask-start");
-maskStartButton.innerHTML = maskIcon;
-maskWrapper.appendChild(maskStartButton);
 const maskStopButton = document.createElement("span");
-maskStopButton.classList.add("uno-mask-stop");
-maskStopButton.innerHTML = maskStopIcon;
 // Close Element
 const closeButton = document.createElement("span");
-closeButton.setAttribute("id", "uno-close");
-wrapper.appendChild(closeButton);
-const appendRecordWrapperToBody: Promise<HTMLElement> = new Promise(resolve => {
-  document.body.appendChild(wrapper);
-  resolve(counter);
-});
+
+const initialInnerElements = () => {
+  // wrapper
+  wrapper.setAttribute("id", "uno-record-widget");
+  wrapper.classList.add("uno-record-wrapper");
+  // Timer element
+  timerWrapper.classList.add("uno-timer");
+  wrapper.appendChild(timerWrapper);
+  counter.classList.add("uno-timer-counter");
+  counter.innerText = "00:00";
+  timerWrapper.appendChild(counter);
+  // Record Elements
+  recordWrapper.classList.add("uno-record");
+  wrapper.appendChild(recordWrapper);
+  startRecordButton.classList.add("uno-record-start");
+  startRecordButton.innerHTML = recordIcon;
+  recordWrapper.appendChild(startRecordButton);
+  stopRecordButton.classList.add("uno-record-stop");
+  stopRecordButton.innerHTML = stopRecordIcon;
+  recordingButton.classList.add("uno-record-recording");
+  recordingButton.innerHTML = recordingIcon;
+  timerWrapper.appendChild(recordingButton);
+  // Mic Element
+  muteWrapper.classList.add("uno-mute");
+  wrapper.appendChild(muteWrapper);
+  muteStartButton.classList.add("uno-mute-start");
+  muteStartButton.innerHTML = muteIcon;
+  muteWrapper.appendChild(muteStartButton);
+  muteStopButton.classList.add("uno-mute-stop");
+  muteStopButton.innerHTML = unmuteIcon;
+  // Mask Element
+  maskWrapper.classList.add("uno-mask");
+  wrapper.appendChild(maskWrapper);
+  maskStartButton.classList.add("uno-mask-start");
+  maskStartButton.innerHTML = maskIcon;
+  maskWrapper.appendChild(maskStartButton);
+  maskStopButton.classList.add("uno-mask-stop");
+  maskStopButton.innerHTML = maskStopIcon;
+  // Close Element
+  closeButton.classList.add("uno-close");
+  closeButton.innerText = "✖";
+  wrapper.appendChild(closeButton);
+};
+const appendRecordWrapperToBody = (): Promise<HTMLElement> =>
+  new Promise(resolve => {
+    initialInnerElements();
+    document.body.appendChild(wrapper);
+    resolve(counter);
+  });
 export const openRecordWidget: OpenRecordWidgetFunction = async (
   startRecord,
   stopRecord,
@@ -89,14 +102,12 @@ export const openRecordWidget: OpenRecordWidgetFunction = async (
     stopMask(true);
   };
   closeButton.onclick = () => {
-    console.log("Close widget");
     closeWidget(true);
     closeRecordWidget();
   };
-  return await appendRecordWrapperToBody;
+  return await appendRecordWrapperToBody().then(response => response);
 };
 export const closeRecordWidget = () => {
-  console.log("Close widget");
   wrapper.remove();
   timerWrapper.remove();
   counter.remove();
