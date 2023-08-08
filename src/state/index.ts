761f2d5ec2c55de06d6b95b4e7b5471b1e9f1b02
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-08-08 12:27:37 +0330

    feat: add state storage

---
+++ src/state/index.ts
@@ -0,0 +1,12 @@
+import {StateType} from "./type";
+
+const state: StateType = {
+  fullName: null,
+  email: null,
+  avatar: null,
+  requestUrl: null,
+  apiKey: null,
+  autoSecretDataAttribute: null
+};
+const {fullName, email, avatar, requestUrl, apiKey, autoSecretDataAttribute} = state;
+export default {fullName, email, avatar, requestUrl, apiKey, autoSecretDataAttribute};
