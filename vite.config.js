8e954eec1a9dbf45b6054e8dd3663db9b5e4f95b
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-08-14 15:43:06 +0330

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import {fileURLToPath} from "url";
-import {defineConfig} from "vite";
+import {fileURLToPath} from 'url';
+import {defineConfig} from 'vite';
 
 export default defineConfig(async () => {
-  const __dirname = fileURLToPath(new URL(".", import.meta.url));
-        return {
-            root: __dirname + "example",
-        };
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  return {
+    root: __dirname + 'example',
+  };
 });
-
