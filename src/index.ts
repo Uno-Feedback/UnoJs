4a0103cd29a9c8ecd5796594392a4794bebc589b
mohammadKarimi <mha.karimi@gmail.com>
2023-08-15 13:21:18 +0330

    encapsulate some configs into Options. Remove some unnecessary variables.

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,98 @@
-import {Options, SubscriptionData, InitializeFunction, ValidationFunction} from "./types";
-import {openRecordWidget} from "./components/recordWidget";
+import { Options, SubscriptionData, InitializeFunction, ValidationFunction } from "./types";
+import { openRecordWidget } from "./components/recordWidget";
 import ScreenMask from "./components/screenMask";
-import {runTimer, stopTimer} from "./components/timer";
-import {Time} from "./components/timer/type";
+import { runTimer, stopTimer } from "./components/timer";
+import { Time } from "./components/timer/type";
 
 class UnoJSBuilder {
-  private options: Options | null;
-  private subscriptionData: SubscriptionData | null;
-  private startButton: HTMLElement | null;
-  private autoSecretKey: string | null;
-  private screenMask;
-  private timerWrapper: HTMLElement | null;
+    private options: Options | null;
+    private screenMask;
+    private timerWrapper: HTMLElement | null;
 
-  constructor() {
-    this.options = null;
-    this.subscriptionData = null;
-    this.startButton = null;
-    this.autoSecretKey = null;
-    this.screenMask = new ScreenMask();
-    this.timerWrapper = null;
-  }
-
-  validateInitialization: ValidationFunction = (startButtonId, subscriptionData, options) => {
-    if (!subscriptionData) {
-      console.error("[uno-js] Subscription data not set.");
-      return false;
+    constructor() {
+        this.options = null;
+        this.screenMask = new ScreenMask();
+        this.timerWrapper = null;
     }
-    if (!startButtonId) {
-      console.error("[uno-js] Start button not set.");
-      return false;
+
+    private validateInitialization: ValidationFunction = (options) => {
+        if (!options.subscriptionData) {
+            console.error("[uno-js] Subscription data not set.");
+            return false;
+        }
+        if (!options.startButtonId) {
+            console.error("[uno-js] Start button not set.");
+            return false;
+        }
+        if (!options?.user) {
+            console.error("[uno-js] User data not set.");
+            return false;
+        }
+        if (!options?.autoSecretKey)
+            console.warn("[uno-js] Auto secret data attribute not set.");
+        return true;
+    };
+
+    startRecord = () => {
+        console.log("start record");
+        runTimer(this.timerWrapper, this.observeTime.bind(this));
+    };
+
+    stopRecord = () => {
+        console.log("stop record");
+        stopTimer();
+    };
+
+    startMask = () => {
+        this.screenMask.start();
+    };
+
+    stopMask = () => {
+        this.screenMask.removeAllElements();
+        this.screenMask.stop();
+    };
+
+    startMute = () => {
+        console.log("stop mute");
+    };
+
+    stopMute = () => {
+        console.log("stop mute");
+    };
+
+    closeWidget = () => {
+        console.info("[uno-js] Widget closed!");
+        this.stopMask();
+        this.stopRecord();
+    };
+
+    observeTime({ seconds }: Time) {
+        if (seconds === 30) {
+            this.stopRecord();
+        }
     }
-    if (!options?.user) {
-      console.error("[uno-js] User data not set.");
-      return false;
-    }
-    if (!options?.autoSecretKey) console.warn("[uno-js] Auto secret data attribute not set.");
-    return true;
-  };
 
-  startRecord = () => {
-    console.log("start record");
-    runTimer(this.timerWrapper, this.observeTime.bind(this));
-  };
-  stopRecord = () => {
-    console.log("stop record");
-    stopTimer();
-  };
-  startMask = () => {
-    this.screenMask.start();
-  };
-  stopMask = () => {
-    this.screenMask.removeAllElements();
-    this.screenMask.stop();
-  };
-  startMute = () => {
-    console.log("stop mute");
-  };
-  stopMute = () => {
-    console.log("stop mute");
-  };
-  closeWidget = () => {
-    console.info("[uno-js] Widget closed!");
-    this.stopMask();
-    this.stopRecord();
-  };
+    initialize: InitializeFunction = (options) => {
+        if (!this.validateInitialization(options)) return;
+        console.info("[uno-js] Package initialized!");
+        this.options = options;
+        let startButton = document.getElementById(options.startButtonId);
 
-  observeTime({seconds}: Time) {
-    if (seconds === 30) {
-      this.stopRecord();
-    }
-  }
-
-  initialize: InitializeFunction = (startButtonId, subscriptionData, options) => {
-    if (!this.validateInitialization(startButtonId, subscriptionData, options)) return;
-
-    console.info("[uno-js] Package initialized!");
-
-    this.options = options;
-    this.autoSecretKey = options.autoSecretKey ?? null;
-    this.subscriptionData = subscriptionData;
-    this.startButton = document.getElementById(startButtonId);
-
-    if (this.startButton)
-      this.startButton.addEventListener("click", () => {
-        // this.startRecord, this.stopRecord, this.startMask, this.stopMask, this.closeWidget
-        openRecordWidget({
-          startRecord: this.startRecord,
-          stopRecord: this.stopRecord,
-          startMask: this.startMask,
-          stopMask: this.stopMask,
-          startMute: this.startMute,
-          stopMute: this.stopMute,
-          closeWidget: this.closeWidget
-        }).then(response => {
-          this.timerWrapper = response;
-        });
-      });
-  };
+        if (startButton)
+            startButton.addEventListener("click", () => {
+                openRecordWidget({
+                    startRecord: this.startRecord,
+                    stopRecord: this.stopRecord,
+                    startMask: this.startMask,
+                    stopMask: this.stopMask,
+                    startMute: this.startMute,
+                    stopMute: this.stopMute,
+                    closeWidget: this.closeWidget
+                }).then(response => {
+                    this.timerWrapper = response;
+                });
+            });
+    };
 }
 
 const unoJS = new UnoJSBuilder();
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,54 +1,50 @@
 export interface Options {
-  user: User;
-  autoSecretKey?: string;
-  callbacks?: Callbacks;
+    user: User;
+    autoSecretKey?: string;
+    callbacks?: Callbacks;
+    startButtonId: string;
+    subscriptionData: SubscriptionData;
 }
 
 export interface User {
-  fullName: string;
-  email: string;
-  avatar?: string;
+    fullName: string;
+    email: string;
+    avatar?: string;
 }
 
 export interface Timer {
-  second: number;
-  minute: number;
-  hour: number;
+    second: number;
+    minute: number;
+    hour: number;
 }
 
 export interface Callbacks {
-  onOpenWidget?: () => void;
-  onCloseWidget?: () => void;
-  onStartMask?: () => void;
-  onStopMask?: () => void;
-  onStartTimer?: (timer: Timer) => void;
-  onStopTimer?: () => void;
-  onStartRecording?: () => void;
-  onStopRecording?: () => void;
-  onSubmit?: () => void;
-  onError?: () => void;
-  onSuccess?: () => void;
+    onOpenWidget?: () => void;
+    onCloseWidget?: () => void;
+    onStartMask?: () => void;
+    onStopMask?: () => void;
+    onStartTimer?: (timer: Timer) => void;
+    onStopTimer?: () => void;
+    onStartRecording?: () => void;
+    onStopRecording?: () => void;
+    onSubmit?: () => void;
+    onError?: () => void;
+    onSuccess?: () => void;
 }
 
 export interface SubscriptionData {
-  apiKey: string;
-  requestUrl: string;
+    apiKey: string;
+    requestUrl: string;
 }
 
 export interface InitializationData {
-  subscriptionData: SubscriptionData;
-  startButtonId: string;
-  options: Options;
+    options: Options;
 }
 
 export type InitializeFunction = (
-  startButtonId: string,
-  subscriptionData: SubscriptionData,
-  options: Options
+    options: Options
 ) => void;
 
 export type ValidationFunction = (
-  startButtonId: string,
-  subscriptionData: SubscriptionData,
-  options: Options
+    options: Options
 ) => boolean;
