a98bb175f0da6ed2076ef4c9e064b77b164483be
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-08-21 21:23:44 +0330

    refactor: remove an unnecessary type

--- src/components/autoSecret/index.ts
+++ src/components/autoSecret/index.ts
@@ -1,45 +1,43 @@
 import optionsState from "./../../state";
 
-export interface ElementInterface { }
-
 const setBlur = (element: HTMLElement): void => {
-    element.style.transition = "all 0.3s ease-in-out";
-    element.style.filter = "blur(5px)";
-    element.style.background = "#CCC";
-    element.style.color = "#CCC";
+  element.style.transition = "all 0.3s ease-in-out";
+  element.style.filter = "blur(5px)";
+  element.style.background = "#CCC";
+  element.style.color = "#CCC";
 };
 
 const removeBlur = (element: HTMLElement): void => {
-    element.style.transition = "";
-    element.style.filter = "";
-    element.style.background = "";
-    element.style.color = "";
+  element.style.transition = "";
+  element.style.filter = "";
+  element.style.background = "";
+  element.style.color = "";
 };
 
 export const startSecret = (): void => {
-    const secretAttributes = document.querySelectorAll(`[data-${optionsState.autoSecretKey}]`);
-    if (!optionsState.autoSecretKey || !secretAttributes) {
-        console.warn("Auto secret attribute is not set!");
-        return;
-    }
-    secretAttributes.forEach(secretAttribute => {
-        const element = secretAttribute as HTMLElement;
-        setBlur(element);
-        secretAttribute.addEventListener("mouseover", () => {
-            removeBlur(element);
-        });
-        secretAttribute.addEventListener("mouseout", () => {
-            setBlur(element);
-        });
+  const secretAttributes = document.querySelectorAll(`[data-${optionsState.autoSecretKey}]`);
+  if (!optionsState.autoSecretKey || !secretAttributes) {
+    console.warn("Auto secret attribute is not set!");
+    return;
+  }
+  secretAttributes.forEach(secretAttribute => {
+    const element = secretAttribute as HTMLElement;
+    setBlur(element);
+    secretAttribute.addEventListener("mouseover", () => {
+      removeBlur(element);
     });
+    secretAttribute.addEventListener("mouseout", () => {
+      setBlur(element);
+    });
+  });
 };
 
 export const endSecret = (): void => {
-    const secretAttributes = document.querySelectorAll(`[data-${optionsState.autoSecretKey}]`);
-    if (!optionsState.autoSecretKey || !secretAttributes) return;
+  const secretAttributes = document.querySelectorAll(`[data-${optionsState.autoSecretKey}]`);
+  if (!optionsState.autoSecretKey || !secretAttributes) return;
 
-    secretAttributes.forEach(secretAttribute => {
-        const element = secretAttribute as HTMLElement;
-        removeBlur(element);
-    });
+  secretAttributes.forEach(secretAttribute => {
+    const element = secretAttribute as HTMLElement;
+    removeBlur(element);
+  });
 };
