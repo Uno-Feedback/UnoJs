71f80bb5dbfaca1cb70fcb6192a5201db9056e0a
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-08-21 21:20:31 +0330

    docs: add some docs

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@
  * It provides the following functions:
  *
  * - `initialize`: This function is used to initialize the library. It takes three arguments:
- * `startButtonId`: The ID of the HTML element that will be used as the start button.
- * `subscriptionData`: An object that contains the subscription data, such as the request URL and API key.
- * `options`: An object that contains the user data, such as the full name, email, and avatar.
+ *   `startButtonId`: The ID of the HTML element that will be used as the start button.
+ *   `subscriptionData`: An object that contains the subscription data, such as the request URL and API key.
+ *   `options`: An object that contains the user data, such as the full name, email, and avatar.
  *
  * - `startRecord`: This function is to start recording.
  * - `stopRecord`: This function is to stop recording.
@@ -19,181 +19,191 @@
  * For more information, please see the [uno-js documentation](https://github.com/Uno-Feedback/UnoJs#readme).
  */
 
-import { Options } from "./types";
-import { closeRecordWidget, openRecordWidget, resetWidget } from "./components/recordWidget";
+import {Options} from "./types";
+import {closeRecordWidget, openRecordWidget, resetWidget} from "./components/recordWidget";
 import ScreenMask from "./components/screenMask";
-import { runTimer, stopTimer } from "./components/timer";
-import { Time } from "./components/timer/type";
+import {runTimer, stopTimer} from "./components/timer";
+import {Time} from "./components/timer/type";
 import optionsState from "./state";
 import Observable from "./components/observable";
-import { endSecret, startSecret } from "./components/autoSecret";
+import {endSecret, startSecret} from "./components/autoSecret";
 import MediaStreamRecorder from "./components/mediaStreamRecorder";
 
+/**
+ * The UnoJSBuilder class is the main entry point for the uno-js package.
+ * It provides methods for starting and stopping recording, as well as for managing the mask and mute features.
+ */
+
 class UnoJSBuilder {
-    private screenMask;
-    private screenRecorder: MediaStreamRecorder | null;
-    private timerWrapper: HTMLElement | null;
-    private audio: boolean;
-    private maskIsRunning: boolean;
-    private recordIsStarted: boolean;
+  private screenMask;
+  private screenRecorder: MediaStreamRecorder | null;
+  private timerWrapper: HTMLElement | null;
+  private audio: boolean;
+  private maskIsRunning: boolean;
+  private recordIsStarted: boolean;
 
-    constructor() {
-        this.screenMask = new ScreenMask();
-        this.screenRecorder = null;
-        this.timerWrapper = null;
-        this.audio = true;
-        this.maskIsRunning = false;
-        this.recordIsStarted = false;
+  constructor() {
+    this.screenMask = new ScreenMask();
+    this.screenRecorder = null;
+    this.timerWrapper = null;
+    this.audio = true;
+    this.maskIsRunning = false;
+    this.recordIsStarted = false;
+  }
+
+  /**
+   Validates the initialization options.
+   @param options The initialization options.
+   @returns Whether the options are valid.
+   */
+
+  validateInitialization = (options: Options): boolean => {
+    if (!options.subscriptionData) {
+      console.error("[uno-js] Subscription data not set.");
+      return false;
     }
+    if (!options.subscriptionData.requestUrl) {
+      console.error("[uno-js] Request url not set.");
+      return false;
+    }
+    if (!options.subscriptionData.requestUrl) {
+      console.error("[uno-js] Api key not set.");
+      return false;
+    }
+    if (!options.startButtonId) {
+      console.error("[uno-js] Start button not set.");
+      return false;
+    }
+    if (!options?.user) {
+      console.error("[uno-js] User data not set.");
+      return false;
+    }
+    if (!options?.autoSecretKey) console.warn("[uno-js] Auto secret data attribute not set.");
 
-    validateInitialization = (options: Options): boolean => {
-        if (!options.subscriptionData) {
-            console.error("[uno-js] Subscription data not set.");
-            return false;
-        }
-        if (!options.subscriptionData.requestUrl) {
-            console.error("[uno-js] Request url not set.");
-            return false;
-        }
-        if (!options.subscriptionData.requestUrl) {
-            console.error("[uno-js] Api key not set.");
-            return false;
-        }
-        if (!options.startButtonId) {
-            console.error("[uno-js] Start button not set.");
-            return false;
-        }
-        if (!options?.user) {
-            console.error("[uno-js] User data not set.");
-            return false;
-        }
-        if (!options?.autoSecretKey)
-            console.warn("[uno-js] Auto secret data attribute not set.");
+    return true;
+  };
 
-        return true;
-    };
+  startRecord = (): void => {
+    if (!this.screenRecorder) return;
 
-    startRecord = (): void => {
-        if (!this.screenRecorder) return;
-
-        this.screenRecorder
-            .start()
-            .then(record => {
-                if (!record) {
-                    resetWidget();
-                    return;
-                }
-                startSecret();
-                this.setRecordState(true);
-                runTimer(this.timerWrapper, this.observeTime);
-                console.info("[uno-js] Record started");
-            })
-            .catch(error => {
-                this.closeWidget();
-                this.setRecordState(false);
-                console.error(`[uno-js] Error while starting record: ${error}`);
-            });
-        Observable.subscribe("closeWidget", this.closeWidget);
-        Observable.subscribe("setRecordState", this.setRecordState);
-    };
-
-    stopRecord = () => {
-        if (!this.screenRecorder || !this.recordIsStarted) return;
-        console.info("[uno-js] Record stopped!");
-        this.screenRecorder.stopRecording();
-        stopTimer();
+    this.screenRecorder
+      .start()
+      .then(record => {
+        if (!record) {
+          resetWidget();
+          return;
+        }
+        startSecret();
+        this.setRecordState(true);
+        runTimer(this.timerWrapper, this.observeTime);
+        console.info("[uno-js] Record started");
+      })
+      .catch(error => {
+        this.closeWidget();
         this.setRecordState(false);
-    };
+        console.error(`[uno-js] Error while starting record: ${error}`);
+      });
+    Observable.subscribe("closeWidget", this.closeWidget);
+    Observable.subscribe("setRecordState", this.setRecordState);
+  };
 
-    startMask = () => {
-        this.screenMask.start();
-        this.maskIsRunning = true;
-        console.info("[uno-js] Mask started!");
-    };
+  stopRecord = () => {
+    if (!this.screenRecorder || !this.recordIsStarted) return;
+    console.info("[uno-js] Record stopped!");
+    this.screenRecorder.stopRecording();
+    stopTimer();
+    this.setRecordState(false);
+  };
 
-    stopMask = () => {
-        if (!this.maskIsRunning) return;
-        this.screenMask.stop();
-        this.maskIsRunning = false;
-        console.info("[uno-js] Mask stopped!");
-    };
+  startMask = () => {
+    this.screenMask.start();
+    this.maskIsRunning = true;
+    console.info("[uno-js] Mask started!");
+  };
 
-    startMute = () => {
-        if (this.recordIsStarted) return;
-        /* todo mute only before start recording */
-        this.audio = false;
-        console.info("[uno-js] Mute started!");
-    };
+  stopMask = () => {
+    if (!this.maskIsRunning) return;
+    this.screenMask.stop();
+    this.maskIsRunning = false;
+    console.info("[uno-js] Mask stopped!");
+  };
 
-    stopMute = () => {
-        this.audio = true;
-        console.info("[uno-js] Mute stopped!");
-    };
+  startMute = () => {
+    if (this.recordIsStarted) return;
+    /* todo mute only before start recording */
+    this.audio = false;
+    console.info("[uno-js] Mute started!");
+  };
 
-    closeWidget = () => {
-        this.stopMask();
-        this.stopRecord();
-        endSecret();
-        stopTimer();
-        closeRecordWidget();
-        console.info("[uno-js] Widget closed!");
-    };
+  stopMute = () => {
+    this.audio = true;
+    console.info("[uno-js] Mute stopped!");
+  };
 
-    setRecordState = (state: boolean) => {
-        console.log({ state });
-        this.recordIsStarted = state;
-    };
+  closeWidget = () => {
+    this.stopMask();
+    this.stopRecord();
+    endSecret();
+    stopTimer();
+    closeRecordWidget();
+    console.info("[uno-js] Widget closed!");
+  };
 
-    observeTime({ minutes }: Time) {
-        if (minutes === optionsState.videoMaxLength) { //TODO: it is magic number, we have to avoid this equallity
-            this.stopRecord();
-        }
+  setRecordState = (state: boolean) => {
+    console.log({state});
+    this.recordIsStarted = state;
+  };
+
+  observeTime({minutes}: Time) {
+    if (minutes === optionsState.videoMaxLength) {
+      //TODO: it is magic number, we have to avoid this equality
+      this.stopRecord();
     }
+  }
 
-    initialize = (options: Options): void => {
-        if (!this.validateInitialization(options)) return;
+  initialize = (options: Options): void => {
+    if (!this.validateInitialization(options)) return;
 
-        const startButton = document.getElementById(options.startButtonId);
+    const startButton = document.getElementById(options.startButtonId);
 
+    // TODO: we should be encapsulated this assignments
+    optionsState.user = options.user;
+    optionsState.autoSecretKey = options.autoSecretKey ?? undefined;
+    optionsState.subscriptionData = options.subscriptionData;
+    options.videoMaxLength ??= options.videoMaxLength;
 
-        //TODO: we should capsulate this assignments
-        optionsState.user = options.user;
-        optionsState.autoSecretKey = options.autoSecretKey ?? undefined;
-        optionsState.subscriptionData = options.subscriptionData;
-        options.videoMaxLength ??= options.videoMaxLength;
+    this.screenRecorder = new MediaStreamRecorder({
+      displayMediaConstraints: {
+        audio: this.audio,
+        video: true
+      },
+      userMediaConstraints: {
+        audio: {
+          sampleSize: 100,
+          frameRate: {max: 30},
+          channelCount: 2
+        }
+      },
+      mimeType: "video/webm"
+    });
 
-        this.screenRecorder = new MediaStreamRecorder({
-            displayMediaConstraints: {
-                audio: this.audio,
-                video: true
-            },
-            userMediaConstraints: {
-                audio: {
-                    sampleSize: 100,
-                    frameRate: { max: 30 },
-                    channelCount: 2
-                }
-            },
-            mimeType: "video/webm"
+    if (startButton)
+      startButton.addEventListener("click", () => {
+        openRecordWidget({
+          onStartRecord: this.startRecord,
+          onStopRecord: this.stopRecord,
+          onStartMask: this.startMask,
+          onStopMask: this.stopMask,
+          onStartMute: this.startMute,
+          onStopMute: this.stopMute,
+          onCloseWidget: this.closeWidget
+        }).then(response => {
+          this.timerWrapper = response;
         });
+      });
 
-        if (startButton)
-            startButton.addEventListener("click", () => {
-                openRecordWidget({
-                    onStartRecord: this.startRecord,
-                    onStopRecord: this.stopRecord,
-                    onStartMask: this.startMask,
-                    onStopMask: this.stopMask,
-                    onStartMute: this.startMute,
-                    onStopMute: this.stopMute,
-                    onCloseWidget: this.closeWidget
-                }).then(response => {
-                    this.timerWrapper = response;
-                });
-            });
-
-        console.info("[uno-js] Package initialized!"); //TODO: after develop these methods should be removed
-    };
+    console.info("[uno-js] Package initialized!"); //TODO: after develop these methods should be removed
+  };
 }
 
 const unoJS = new UnoJSBuilder();
